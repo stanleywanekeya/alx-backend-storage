eb8ea9948529bc5b0dee93ff8a9f8a3345f4b65d
stanleywanekeya <stanleywanekeya123@gmail.com>
2024-05-11 14:14:19 +0300

    Advanced sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Creates a table with unique users.
-Drop TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
